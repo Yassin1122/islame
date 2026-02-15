aca449cbc673f3deadb18f6f67004b4f43e5f443
Yassin <yassin1122004@gmail.com>
2026-02-15 02:53:50 +0200

    added splash screen.

--- lib/home/home-screen.dart
+++ lib/home/home-screen.dart
@@ -6,6 +6,8 @@ static const String  routeName='HomeScreen';
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:islame/home/home-screen.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
-void main(){
-runApp(MyApp());
+
+  void main() {
+    WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+    // FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding); // Removing this because we aren't waiting for anything specific yet
+    
+    runApp(MyApp());
 }
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -13,6 +18,7 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       initialRoute:HomeScreen.routeName,
       routes:{
+
         HomeScreen.routeName:(context)=>HomeScreen()
       },
     );
